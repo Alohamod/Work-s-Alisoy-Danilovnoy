fa210548b7e4810c31509ef84a249ccfe2f309e5
Alohamod <airat.grafon@gmail.com>
2026-04-23 18:01:45 +0300

    Add files via upload

---
+++ 23.04.cs
@@ -0,0 +1,39 @@
+﻿namespace Lab10
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            string fileName = "example.txt";
+            string fullPath = Path.Combine(folderPath, fileName);
+            File.Create(fullPath).Close();
+            File.WriteAllText(fullPath, "Hello");
+            File.AppendAllText(fullPath, "world");
+            string[] joi = new string[] {"rrf","rfr","wqwer"};
+            File.WriteAllLines(fullPath, joi);
+            string con = File.ReadAllText(fullPath);
+            string[] con_ar = File.ReadAllLines(fullPath);
+            Console.WriteLine(con);
+            Console.WriteLine(string.Join("\n", con_ar));
+
+            string folderPath1 = Path.Combine(folderPath, "EXAMPl");
+            string folderPath2 = Path.Combine(folderPath1, "anothexamp");
+            if (!Directory.Exists(folderPath1))
+            {
+                Directory.CreateDirectory(folderPath1);
+                if (!File.Exists(folderPath2))
+                    {
+                    File.Create(folderPath2).Close(); 
+                }
+            }
+            string ext = "txt";
+            string folderPath3 = Path.GetDirectoryName(folderPath2);
+            string fileName1 = Path.GetFileName(folderPath2);
+            Console.Write(fileName1);
+            
+
+        }
+    }
+}
